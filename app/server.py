553c7651162cd799b0c80735a950184a0cd4e698
ada-h <adanaza89@gmail.com>
2020-04-20 04:52:28 -1200

    potholes or good roads

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/open?id=1JJ1erbAdCewrTzB_G7N66YATnmIf3FRY'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['goodroads', 'potholes']
 path = Path(__file__).parent
 
 app = Starlette()
